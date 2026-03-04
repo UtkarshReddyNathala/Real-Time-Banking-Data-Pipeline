7569517a1fbbebdfe7c25c2df18ab48990159b46
UtkarshReddyNathala <utkarshnathala@gmail.com>
2026-03-04 10:44:28 +0530

    Update retry delay to 20 minutes
    Increased the retry delay from 1 minute to 20 minutes.

--- docker/dags/scd_snapshots.py
+++ docker/dags/scd_snapshots.py
@@ -6,7 +6,7 @@ default_args = {
     "owner": "airflow",
     "depends_on_past": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=1),
+    "retry_delay": timedelta(minutes=20),
 }
 
 with DAG(
